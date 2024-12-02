069127653b52551e390b27d37507e96c1f6e3568
SirOlash <Olasupoemmanuel30@gmail.com>
2024-12-02 14:47:23 +0100

    task two

--- phaseGateOne/task_two_sum_of_integer.py
+++ phaseGateOne/task_two_sum_of_integer.py
@@ -1,3 +1,13 @@
+"""prompt user to enter a number from 0 - 1000
+save the number as "user_input"
+make a while loop that runs provided the user input is greater than 0
+calculate last number by user_input % 10 in the loop
+then add the value to sum which is originaly zero
+then use a floor division to divide the user input by 10
+this goes on till the user input is less than 0"""
+
+
+
 user_input = int(input("Enter an integer between 0-1000: "))
 
 sum = 0
